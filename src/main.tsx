840fe6d2fe6697256fa3b9bdad8f1a9c94f8b870
Andrei Karas <38630949+Justbigmack@users.noreply.github.com>
2026-01-14 22:30:23 +0100

    fix incorrect alias

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import App from "./App.tsx";
-import { BasketProvider } from "@/context/BasketContext";
+import { BasketProvider } from "@context/BasketContext";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
